342b77b328b724299df99c4b1e1ba6aaaa03e457
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-01 14:50:06 -0500

    Installed and configured big calendar in project

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 import { NavBar } from '../ui/NavBar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+	{
+		title: 'cumpleaños del jefe',
+		start: moment().toDate(),
+		end: moment().add(2, 'hours').toDate(),
+		bgcolor: '#FAFAFA',
+	},
+];
 
 export const CalendarScreen = () => {
 	return (
-		<div>
+		<div className="calendar-screen">
 			<NavBar />
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+				style={{ height: 500 }}
+			/>
 		</div>
 	);
 };
