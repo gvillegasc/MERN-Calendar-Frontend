df22eff499139aa7f9742257b368dedbe202e6b8
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-02 09:36:10 -0500

    Getting form data

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -23,17 +23,38 @@ export const CalendarModal = () => {
 	const [dateStart, setDateStart] = useState(now.toDate());
 	const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
 
-	// const [isOpen, setIsOpen] = useState(true);
-	const closeModal = () => {
-		// setIsOpen(false);
+	const [formValues, setFormValues] = useState({
+		title: 'Evento',
+		notes: '',
+		start: now.toDate(),
+		end: nowPlus1.toDate(),
+	});
+
+	const { title, notes } = formValues;
+
+	const handleInputChange = ({ target }) => {
+		setFormValues({
+			...formValues,
+			[target.name]: target.value,
+		});
 	};
+
+	const closeModal = () => {};
 
 	const handleStartDateChange = (e) => {
 		setDateStart(e);
+		setFormValues({
+			...formValues,
+			start: e,
+		});
 	};
 
 	const handleEndDateChange = (e) => {
 		setDateEnd(e);
+		setFormValues({
+			...formValues,
+			end: e,
+		});
 	};
 
 	return (
@@ -76,6 +97,8 @@ export const CalendarModal = () => {
 						placeholder="Título del evento"
 						name="title"
 						autoComplete="off"
+						value={title}
+						onChange={handleInputChange}
 					/>
 					<small id="emailHelp" className="form-text text-muted">
 						Una descripción corta
@@ -89,6 +112,8 @@ export const CalendarModal = () => {
 						placeholder="Notas"
 						rows="5"
 						name="notes"
+						value={notes}
+						onChange={handleInputChange}
 					></textarea>
 					<small id="emailHelp" className="form-text text-muted">
 						Información adicional
