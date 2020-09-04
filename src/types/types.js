6f442b07aad55b4b614cf3e96e3df44930ab4560
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-04 13:23:36 -0500

    Creando nuevos types

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
 	eventClearActiveEvent: '[Event] Clear active event',
 	eventUpdated: '[Event] Event updated',
 	eventDeleted: '[Event] Event deleted',
+
+	authChecking: '[Auth] Checking login state',
+	authCheckingFinish: '[Auth] Finish checking login state',
+	authStartLogin: '[Auth] Start login',
+	authLogin: '[Auth] Login',
+	authStartRegister: '[Auth] Start Register',
+	authStartTokenRenew: '[Auth] Start token renew',
+	authLogout: '[Auth] Logout',
 };
